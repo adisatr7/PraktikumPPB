e3bc34646fa91125a538a6b86529a74cedbe9b7c
RaddOx7 <19104027@ittelkom-pwt.ac.id>
2022-01-24 20:57:29 +0700

    Modul6

---
+++ app/src/main/java/com/raddox7_19104027/Modul2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.raddox7_19104027.Modul2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/raddox7_19104027/Modul6/MyData.kt
@@ -0,0 +1,7 @@
+package com.raddox7_19104027.Modul6
+
+data class MyData(
+    var name : String,
+    var description : String,
+    var photo : String
+)
